5a37c433042d4697233d3a0d08f0188b6f25d041
Edubmstr <153729455+Edubmstr@users.noreply.github.com>
2024-02-21 22:39:30 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+dotenv.config();
+const port = process.env.PORT || 8090
+
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
+app.get('/data', (req, res) => {
+   const result = pool.query('SELECT * FROM dox WHERE id=(SELECT max(id) FROM dox)', function(err, rows, fields){
+      if(err) throw err;
+      res.send(rows);
+   });
+});
+
+app.get('/insert/:amount', (req, res) => {
+   const result = pool.query(`INSERT into dox (money) VALUES(${req.params.amount})`, function(err, rows, fields){
+      if(err) throw err;
+      res.send(rows);
+   });
+})
+
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`)
+ });
